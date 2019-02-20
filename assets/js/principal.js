f90c079f62157bcd5c4c3e7837255a40ab45ab8d
MuriloEduardo <muriloeduardoooooo@gmail.com>
2019-02-20 09:36:10 -0300

    icones dos contatos no browser

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -5,4 +5,16 @@ $(function () {
     $('#menu-toggle').click(function () {
         $('#wrapper').toggleClass('toggled');
     });
+
+    $('.dataTable').on('shown.bs.collapse hidden.bs.collapse', '.contatos-filtrados .collapse', function () {
+        var $this = $(this),
+            $contaotsFiltrados = $this.parents('.contatos-filtrados'),
+            $btn = $contaotsFiltrados.find('.btn');
+
+        if ($this.hasClass('show')) {
+            $btn.find('i.fas').removeClass('fa-plus-circle').addClass('fa-minus-circle');
+        } else {
+            $btn.find('i.fas').removeClass('fa-minus-circle').addClass('fa-plus-circle');
+        }
+    });
 });
