47d788bfb32e44d2a70276b50ab8b68208313557
vicentegalle <vicentegalle@gmail.com>
2019-03-26 20:04:06 -0300

    Anurag - relatorio FlxCx rodando

---
+++ assets/js/relatoriofluxocaixa.js
@@ -0,0 +1,183 @@
+function floatParaPadraoBrasileiro(valor){
+    var valortotal = valor;
+    valortotal = number_format(valortotal,2,',','.');
+    return valortotal;
+}
+
+function floatParaPadraoInternacional(valor){
+    
+    var valortotal = valor;
+    valortotal = valortotal.replace(".", "").replace(".", "").replace(".", "").replace(".", "");
+    valortotal = valortotal.replace(",", ".");
+    valortotal = parseFloat(valortotal).toFixed(2);
+    return valortotal;
+}
+
+function number_format( numero, decimal, decimal_separador, milhar_separador ){ 
+        numero = (numero + '').replace(/[^0-9+\-Ee.]/g, '');
+        var n = !isFinite(+numero) ? 0 : +numero,
+            prec = !isFinite(+decimal) ? 0 : Math.abs(decimal),
+            sep = (typeof milhar_separador === 'undefined') ? ',' : milhar_separador,
+            dec = (typeof decimal_separador === 'undefined') ? '.' : decimal_separador,
+            s = '',
+            toFixedFix = function (n, prec) {
+                var k = Math.pow(10, prec);
+                return '' + Math.round(n * k) / k;
+            };
+ 
+        // Fix para IE: parseFloat(0.55).toFixed(0) = 0;
+        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+        if (s[0].length > 3) {
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+        }
+        if ((s[1] || '').length < prec) {
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1).join('0');
+        }
+        return s.join(dec);
+}
+
+$(function () {
+
+    var $collapse = $('#collapseFluxocaixaResumo'),
+        $cardBodyFiltros = $('#card-body-filtros'),
+        dataTable = window.dataTable,
+        indexColumns = {
+            acoes: 0,
+            movimentacoes: 1,
+            data_operacao: 4,
+            valor_total: 5,
+            data_vencimento: 6,
+            status: 16,
+            observacao: 21
+        }
+    
+    
+    dataTable.page.len(-1).draw();
+    
+
+    function resumo () {
+        
+        var rowData = dataTable.rows().data(),
+        somasDespesasQ = 0,
+        somasReceitasQ = 0,
+        somasDespesasAQ = 0,
+        somasReceitasAQ = 0,
+        resultadoRealizado = 0,
+        resultadoARealizar = 0,
+        numReceitasQ = 0,
+        numReceitasAQ = 0,
+        numDespesasQ = 0,
+        numDespesasAQ = 0,
+        totalQ = 0,
+        totalAQ = 0,
+        previsao = 0;
+    
+    
+        i = 0;
+        rowData.each(function () {
+            if (rowData[i][indexColumns.movimentacoes].toLowerCase() == 'despesa') {
+                               
+                var despesa = rowData[i][indexColumns.valor_total];
+                despesa = despesa.replace('R$  ', '');
+                despesa = floatParaPadraoInternacional(despesa);
+
+                if (rowData[i][indexColumns.status].toLowerCase() == 'quitado'){
+                    somasDespesasQ += parseFloat(despesa);
+                    numDespesasQ +=1;
+                } else {
+                    somasDespesasAQ += parseFloat(despesa);
+                    numDespesasAQ +=1;
+                };
+            }
+            i++;
+        });
+
+        i = 0;
+        rowData.each(function () {
+            if (rowData[i][indexColumns.movimentacoes].toLowerCase() == 'receita') {
+                               
+                var receita = rowData[i][indexColumns.valor_total];
+                receita = receita.replace('R$  ', '');
+                receita = floatParaPadraoInternacional(receita);
+
+                if (rowData[i][indexColumns.status].toLowerCase() == 'quitado'){
+                    somasReceitasQ += parseFloat(receita);
+                    numReceitasQ +=1 ;
+                } else {
+                    somasReceitasAQ += parseFloat(receita);
+                    numReceitasAQ +=1 ;
+                };
+            }
+            i++;
+        });
+
+        resultadoRealizado = somasReceitasQ - somasDespesasQ;
+        resultadoARealizar = somasReceitasAQ - somasDespesasAQ;
+
+        previsaoReceitas = somasReceitasQ + somasReceitasAQ;
+        previsaoDespesas = somasDespesasQ + somasDespesasAQ;
+        previsaoResultados = resultadoRealizado + resultadoARealizar;
+
+        totalQ = numReceitasQ + numDespesasQ;
+        totalAQ = numReceitasAQ + numDespesasAQ;
+        total = totalQ + totalAQ;
+       
+
+        $('#despesasQuitadas').text(floatParaPadraoBrasileiro(somasDespesasQ));
+        $('#despesasAQuitar').text(floatParaPadraoBrasileiro(somasDespesasAQ));
+
+        $('#receitasQuitadas').text(floatParaPadraoBrasileiro(somasReceitasQ));
+        $('#receitasAQuitar').text(floatParaPadraoBrasileiro(somasReceitasAQ));
+
+        if(resultadoRealizado <= 0){
+            $('#cardResultadoRealizado').removeClass('bg-success text-white');
+            $('#cardResultadoRealizado').addClass('bg-danger text-white');
+        } else{
+            $('#cardResultadoRealizado').removeClass('bg-danger text-white');
+            $('#cardResultadoRealizado').addClass('bg-success text-white');
+        }
+
+        if(resultadoARealizar <= 0){
+            $('#cardResultadoARealizar').removeClass('bg-success text-white');
+            $('#cardResultadoARealizar').addClass('bg-danger text-white');
+        } else{
+            $('#cardResultadoARealizar').removeClass('bg-danger text-white');
+            $('#cardResultadoARealizar').addClass('bg-success text-white');
+        }
+
+        if(previsaoResultados <= 0){
+            $('#cardPrevisaoResultados').removeClass('bg-success text-white');
+            $('#cardPrevisaoResultados').addClass('bg-danger text-white');
+        } else{
+            $('#cardPrevisaoResultados').removeClass('bg-danger text-white');
+            $('#cardPrevisaoResultados').addClass('bg-success text-white');
+        }
+
+        $('#resultadoRealizado').text(floatParaPadraoBrasileiro(resultadoRealizado));
+        $('#resultadoARealizar').text(floatParaPadraoBrasileiro(resultadoARealizar));
+
+        $('#previsaoResultados').text(floatParaPadraoBrasileiro(previsaoResultados));
+        $('#previsaoReceitas').text(floatParaPadraoBrasileiro(previsaoReceitas));
+        $('#previsaoDespesas').text(floatParaPadraoBrasileiro(previsaoDespesas));
+
+        $('[data-id=total]').text(parseInt(total));
+        $('[data-id=totalQ]').text(parseInt(totalQ));
+        $('[data-id=totalAQ]').text(parseInt(totalAQ));
+  
+        
+    };
+
+    $('#collapseFluxocaixaResumo').on('show.bs.collapse', function () {
+        resumo();
+      });
+
+    $('#botaoRelatorio, #limpar-filtro').on('click', function () {
+        $('#collapseFluxocaixaResumo').collapse('hide');
+    });
+
+    $('#card-body-filtros').on('change', function () {
+        $('#collapseFluxocaixaResumo').collapse('hide');
+    });
+      
+});
--- views/_header_browser_relatorios.php
+++ views/_header_browser_relatorios.php
@@ -17,11 +17,15 @@
 <header class="pt-4 pb-5"> <!-- Cabeçalho -->
     <div class="row align-items-center"> <!-- Alinhar as linhas -->
         <div class="col-lg"> <!--Colunas da esquerda -->
-            <h1 class="display-4 text-capitalize font-weight-bold"><?php echo isset($labelTabela["labelBrowser"]) && !empty($labelTabela["labelBrowser"]) ? $labelTabela["labelBrowser"] : $modulo ?></h1>
+            <?php if($modulo == "relatoriofluxocaixa"): ?>
+                <h1 class="display-4 text-capitalize font-weight-bold">Relatório de Fluxo de Caixa</h1>
+            <?php else : ?>
+                <h1 class="display-4 text-capitalize font-weight-bold"><?php echo isset($labelTabela["labelBrowser"]) && !empty($labelTabela["labelBrowser"]) ? $labelTabela["labelBrowser"] : $modulo ?></h1>
+            <?php endif ?>
         </div>
-        <div class="col-lg"> 
+        <!-- <div class="col-lg"> 
             <div class="input-group mb-3 mb-lg-0">
-                <!--Input com os dados da tabela-->
+
                 <input type="text" name="searchDataTable" id="searchDataTable" aria-label="Pesquise por qualquer campo..." class="form-control" placeholder="Pesquise por qualquer campo..." aria-describedby="search-addon">
                 <div class="input-group-append">
                     <span class="input-group-text" id="search-addon">
@@ -29,8 +33,9 @@
                     </span>
                 </div>
             </div>
-        </div>
-        <div class="col-lg flex-lg-grow-0">
+        </div> -->
+
+        <!-- <div class="col-lg flex-lg-grow-0">
             <div class="btn-group-toggle" data-toggle="buttons">
                 <label class="btn btn-info btn-block cursor-pointer" data-toggle="collapse" data-target="#collapseFiltros" aria-expanded="false" aria-controls="collapseFiltros">
                     <input type="checkbox" checked autocomplete="off">
@@ -40,9 +45,9 @@
                     </div>
                 </label>
             </div>
-        </div>
+        </div> -->
 
-        <div class="col-lg flex-lg-grow-0">
+        <!-- <div class="col-lg flex-lg-grow-0">
             <div class="btn-group-toggle" data-toggle="buttons">
                 <label class="btn btn-warning btn-block cursor-pointer" data-toggle="collapse" data-target="#collapseGraficos" aria-expanded="false" aria-controls="collapseGraficos">
                     <input type="checkbox" checked autocomplete="off">
@@ -52,106 +57,89 @@
                     </div>
                 </label>
             </div>
-        </div>
+        </div> -->
 
-        <div class="col-lg flex-lg-grow-0">
-            <div class="btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-warning btn-block cursor-pointer" data-toggle="collapse" data-target="#collapseGraficos2" aria-expanded="false" aria-controls="collapseGraficos2">
-                    <input type="checkbox" checked autocomplete="off">
-                    <div class="d-flex align-items-center">
-                        <i class="fas fa-chart-pie mr-2"></i>
-                        <span>Gráficos2</span>
-                    </div>
-                </label>
-            </div>
-        </div>
 
     </div>
-    <div class="collapse" id="collapseFiltros">
         <div class="card mt-4">
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h5 class="m-0">Filtros</h5>
-                <div class="dropdown">
-                    <button class="btn btn-light dropdown-toggle no-icon-dropdown" type="button" id="dropdownMenuAcoesFiltros" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-h"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuAcoesFiltros">
-                        <button id="limpar-filtro" type="button" class="dropdown-item">
-                            <i class="fas fa-eraser mr-2"></i>
-                            <span>Limpar Filtros</span>
-                        </button>
-                        <button id="criar-filtro" type="button" class="dropdown-item">
-                            <i class="fas fa-plus-circle mr-2"></i>
-                            <span>Adicionar Filtros</span>
-                        </button>
-                    </div>
+                <div>
+                    <button id="limpar-filtro" type = "button" class="btn cursor-pointer btn-outline-secondary btn-sm">Limpar Filtros</button>
+                    <button id="criar-filtro" type="button" class="btn cursor-pointer btn-outline-success btn-sm">Mais Filtros</button>
+                    <button id="graficos"  type="button" class="btn btn-warning  cursor-pointer btn-sm" data-toggle="collapse" data-target="#collapseGraficos2">Gráficos</button>
+                           
+                    <button class="btn btn-primary btn-sm" id="botaoRelatorio" data-toggle="collapse" data-target="#collapseFluxocaixaResumo" aria-expanded="false" aria-controls="collapseFluxocaixaResumo">Gerar Relatório </button>
                 </div>
             </div>
-            <form id="card-body-filtros" class="card-body pb-1">
-                <input type="reset" class="d-none" id="limpar-filtro">
-                <?php if ($modulo == "fluxocaixa"): ?>
-                    <div class="row mb-3">
-                        <div class="col">
-                            <fieldset>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" name="movimentacao" data-index="2" id="todosCheckReceita" value="receita">
-                                    <label class="form-check-label" for="todosCheckReceita">Receita</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" name="movimentacao" data-index="2" id="todosCheckDespesa" value="despesa">
-                                    <label class="form-check-label" for="todosCheckDespesa">Despesa</label>
-                                </div>
-                            </fieldset>
+
+            <div class="collapse show" id="collapseFiltros">
+                <form id="card-body-filtros" class="card-body pb-1">
+                    <input type="reset" class="d-none" id="limpar-filtro">
+                    <?php if ($modulo == "fluxocaixa"): ?>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <fieldset>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" name="movimentacao" data-index="2" id="todosCheckReceita" value="receita">
+                                        <label class="form-check-label" for="todosCheckReceita">Receita</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" name="movimentacao" data-index="2" id="todosCheckDespesa" value="despesa">
+                                        <label class="form-check-label" for="todosCheckDespesa">Despesa</label>
+                                    </div>
+                                </fieldset>
+                            </div>
                         </div>
-                    </div>
-                <?php endif ?>
-                <div class="filtros-wrapper">
-                    <div class="filtros row mb-3">
-                        <div class="filtros-faixa col-lg">
-                            <div class="input-group">
-                                <select class="custom-select input-filtro-faixa">
-                                    <option selected disabled>Filtrar por...</option>
-                                    <?php for($j = 1; $j < count($colunas); $j++): ?>
-                                        <?php if($colunas[$j]["Comment"]["ver"] == "true"): ?>
-                                            <?php if(array_key_exists("label", $colunas[$j]["Comment"]) && array_key_exists("filtro_faixa", $colunas[$j]["Comment"]) && $colunas[$j]["Comment"]["filtro_faixa"]): ?>
-                                                <option value="<?php echo $indexFiltroFaixa ?>" data-tipo="<?php echo $colunas[$j]["Type"] ?>" data-mascara="<?php echo $colunas[$j]["Comment"]["mascara_validacao"] ?>">
-                                                    <?php echo array_key_exists("label", $colunas[$j]["Comment"]) ? $colunas[$j]["Comment"]["label"] : $colunas[$j]["Field"] ?>
-                                                </option>
+                    <?php endif ?>
+                    <div class="filtros-wrapper">
+                        <div class="filtros row mb-3">
+                            <div class="filtros-faixa col-lg">
+                                <div class="input-group">
+                                    <select class="custom-select input-filtro-faixa">
+                                        <option selected disabled value="">Filtrar por...</option>
+                                        <?php for($j = 1; $j < count($colunas); $j++): ?>
+                                            <?php if($colunas[$j]["Comment"]["ver"] == "true"): ?>
+                                                <?php if(array_key_exists("label", $colunas[$j]["Comment"]) && array_key_exists("filtro_faixa", $colunas[$j]["Comment"]) && $colunas[$j]["Comment"]["filtro_faixa"]): ?>
+                                                    <option value="<?php echo $indexFiltroFaixa ?>" data-tipo="<?php echo $colunas[$j]["Type"] ?>" data-mascara="<?php echo $colunas[$j]["Comment"]["mascara_validacao"] ?>">
+                                                        <?php echo array_key_exists("label", $colunas[$j]["Comment"]) ? $colunas[$j]["Comment"]["label"] : $colunas[$j]["Field"] ?>
+                                                    </option>
+                                                <?php endif ?>
+                                                <?php $indexFiltroFaixa++ ?>
                                             <?php endif ?>
-                                            <?php $indexFiltroFaixa++ ?>
-                                        <?php endif ?>
-                                    <?php endfor ?>
-                                </select>
-                                <input type="text" class="form-control input-filtro-faixa min" placeholder="de...">
-                                <input type="text" class="form-control input-filtro-faixa max" placeholder="até...">
+                                        <?php endfor ?>
+                                    </select>
+                                    <input type="text" class="form-control input-filtro-faixa min" placeholder="de...">
+                                    <input type="text" class="form-control input-filtro-faixa max" placeholder="até...">
+                                </div>
                             </div>
-                        </div>
-                        <div class="filtros-texto col-lg">
-                            <div class="input-group">
-                                <select class="custom-select input-filtro-texto">
-                                    <option selected disabled>Filtrar por...</option>
-                                    <?php for($m = 1; $m < count($colunas); $m++): ?>
-                                        <?php if ($colunas[$m]["Comment"]["ver"] == "true"): ?>
-                                            <?php if(!array_key_exists("filtro_faixa", $colunas[$m]["Comment"])): ?>
-                                                <option value="<?php echo $indexFiltroTexto ?>" data-tipo="<?php echo $colunas[$m]["Type"] ?>">
-                                                    <?php echo array_key_exists("label", $colunas[$m]["Comment"]) ? $colunas[$m]["Comment"]["label"] : $colunas[$m]["Field"] ?>
-                                                </option>
-                                            <?php endif ?> 
-                                            <?php $indexFiltroTexto++ ?>
-                                        <?php endif ?>
-                                    <?php endfor ?>
-                                </select>
-                                <input type="text" class="form-control input-filtro-texto texto">
+                            <div class="filtros-texto col-lg">
+                                <div class="input-group">
+                                    <select class="custom-select input-filtro-texto">
+                                        <option selected disabled value="">Filtrar por...</option>
+                                        <?php for($m = 1; $m < count($colunas); $m++): ?>
+                                            <?php if ($colunas[$m]["Comment"]["ver"] == "true"): ?>
+                                                <?php if(!array_key_exists("filtro_faixa", $colunas[$m]["Comment"])): ?>
+                                                    <option value="<?php echo $indexFiltroTexto ?>" data-tipo="<?php echo $colunas[$m]["Type"] ?>">
+                                                        <?php echo array_key_exists("label", $colunas[$m]["Comment"]) ? $colunas[$m]["Comment"]["label"] : $colunas[$m]["Field"] ?>
+                                                    </option>
+                                                <?php endif ?> 
+                                                <?php $indexFiltroTexto++ ?>
+                                            <?php endif ?>
+                                        <?php endfor ?>
+                                    </select>
+                                    <input type="text" class="form-control input-filtro-texto texto">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg flex-grow-0 col-excluir-linha">
-                            <button class="btn btn-danger btn-block" id="excluir-linha">
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
+                            <div class="col-lg flex-grow-0 col-excluir-linha">
+                                <button class="btn btn-danger btn-block" id="excluir-linha">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>  
     </div>
+
 </header>
