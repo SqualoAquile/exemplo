fa1ae740743fb8fc9093c21cb1effaaf5f7f210a
MuriloEduardo <muriloeduardoooooo@gmail.com>
2019-04-27 10:07:25 -0300

    deploy

--- models/Shared.php
+++ models/Shared.php
@@ -696,6 +696,9 @@ class Shared extends model {
                         $array[$chave] = addslashes($registro[$chave]);
                     } 
 
+                } else {
+                    echo $nomeColunas[$i]['Field'] . "<br>";
+                    echo $chave . "<br><br>";
                 }
                 $i++;
             }
