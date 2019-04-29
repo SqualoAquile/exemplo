11f91343c9a72679dcff040aa476fbe4ec93b698
MuriloEduardo <muriloeduardoooooo@gmail.com>
2019-04-29 16:20:30 -0300

    teste

--- assets/js/validacoes.js
+++ assets/js/validacoes.js
@@ -184,6 +184,7 @@ $(function () {
                 if ($this.attr('data-anterior') != $this.val()) {
                     console.log($this.attr('data-unico'))
                     if ($this.attr('data-unico')) {
+                        console.log('if')
                         $this.unico(function (json) {
                             if (!json.length) {
                                 // Não existe, pode seguir
