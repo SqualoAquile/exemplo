2539cc4f23806e2fc757e88fa5b03c374e71de90
MuriloEduardo <muriloeduardoooooo@gmail.com>
2019-05-06 14:35:04 -0300

    alterando texto do botao de imprimir os e orçamento

--- views/_modal_configuracoes_impressao.php
+++ views/_modal_configuracoes_impressao.php
@@ -10,7 +10,9 @@
             <div class="modal-body">
                 <div class="row">
                     <div class="col">
+                        <?php $txtBtnImprimir = "Imprimir e Aprovar" ?>
                         <?php if ($modulo !='ordemservico'): ?>
+                            <?php $txtBtnImprimir = "Imprimir" ?>
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="checkbox" checked="checked" name="checkMedidas" id="checkMedidas" value="medidas">
                                 <label class="form-check-label" for="checkMedidas">Medidas</label>
@@ -55,7 +57,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
-                <button type="submit" class="btn btn-warning">Imprimir e Aprovar</button>
+                <button type="submit" class="btn btn-warning"><?php echo $txtBtnImprimir ?></button>
             </div>
         </form>
     </div>
